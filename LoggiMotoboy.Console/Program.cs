d5c60f931ce5a2244badbf1f80270d4918e549ff
Guilherme Gentile <guigass@live.co.uk>
2020-10-07 13:08:23 -0300

    .

--- LoggiMotoboy.Console/Program.cs
+++ LoggiMotoboy.Console/Program.cs
@@ -10,55 +10,7 @@ namespace LoggiMotoboy.Console
     {
         static void Main(string[] args)
         {
-            using (var loggi = new LoggiPrestoClient("paulo.gentile@exclusivasex.com.br", "loggi2020", "https://staging.loggi.com/graphql"))
-            {
-                var reorder = loggi.RedoOrder(178210).Result;
 
-                return;
-
-                var shops = loggi.AllShops().Result;
-
-                var packages = new List<Package>();
-                packages.Add(new Package
-                {
-                    Recipient = new Recipient
-                    {
-                        Name = "Guilherme",
-                        Phone = "11999960923"
-                    },
-                    Dimensions = new Dimensions
-                    {
-                        Height = 10,
-                        Length = 10,
-                        Weight = 10,
-                        Width = 10
-                    },
-                    Address = new PackageAddress
-                    {
-                        Address = $"Rua Jacirendi, 91 - Tatuapé, São Paulo - SP, Brasil",
-                        Complement = "Apto 152a"
-                    },
-                });
-
-                var pickups = new List<Pickup>();
-                pickups.Add(new Pickup
-                {
-                    Address = new PickupAddress
-                    {
-                        Address = $"Rua Belem, 234 - Belenzinho, São Paulo - SP, Brasil"
-                    }
-                });
-
-                var request = new CreateOrderInput
-                {
-                    ShopId = 5306,
-                    TrackingKey = "1234",
-                    Packages = packages,
-                    Pickups = pickups
-                };
-
-                var respLoggi = loggi.CreateOrder(request).Result;
-            }
         }
     }
 }
